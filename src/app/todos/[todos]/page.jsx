5ce6f5a270decd71e0ebc3d6f6c002b7e5670094
DwashKarma <dwaskc@gmail.com>
2024-02-19 11:44:40 +0545

    feat: get the userDetails from todo's ID

--- src/app/todos/[todos]/page.jsx
+++ src/app/todos/[todos]/page.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+"use client";
+
+import instance from "@/config/axios.config";
+import React, { useEffect, useState } from "react";
 
 function TodosList({ params }) {
+  const [data, setData] = useState([]);
   console.log(params);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const usersData = await instance.get("/users");
+        console.log(usersData?.data?.users[params.todos]);
+        setData(usersData?.data?.users[params.todos]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [params]);
   return (
     <div>
       <h1>Dynamic Todos</h1>
-      <p>ID:{params.todos}</p>
-     
+      <p>ID:{data.id}</p>
+      <p>FirstName:{data.firstName}</p>
+      <p>LastName:{data.lastName}</p>
     </div>
   );
 }
